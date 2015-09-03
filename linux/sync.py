dd4ff79d6b1cb55a50cf8a664624d29c7725074a
Andrea Calzavacca <paranoid.nemo@gmail.com>
2015-09-03 14:09:10 +0200

    add first two sync functions

--- linux/sync.py
+++ linux/sync.py
@@ -20,12 +20,13 @@
 #Import std module(s)
 import os
 import argparse
+import shutil
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--server', help='Define the server directory')
 parser.add_argument('-c', '--client', help='Define the client directory')
 parser.add_argument('-f', '--force', action=store-true, default=false, help='Overwrite the file(s) in the destination dir if exists')
-parser.add_argument('-e', '--extension', action=store-false, default=true, help="Check the extension of the files to compare them and verify if there are the same or not. If the flag are specified the script doesn't check the extension and could operate the comparision only whit the actual file name. Use carefully")
+parser.add_argument('--extension', action=store-false, default=true, help="Check the extension of the files to compare them and verify if they are the same or not. If the flag are specified the script doesn't check the extension and operate the comparision only whit the actual file name. Use carefully")
 args = parser.parse_args()
 
 class Operator():
@@ -34,18 +35,34 @@ class Operator():
     #cancella i file non esistenti nella cartella dist
 
     def verify():
-    # verifica presenza di file nelle due cartelle
+    # verifica presenza di file uguali nelle due cartelle
 
 
 class Sync(Operator):
 
-    def client_sync(dir1, dir2):
-    # verificare esistenza di dir 1 e 2
-    # verificare quali file esistono già in dir 2
-    # copiare i file che esistono solo in dir1 in dir2
+    def client_sync(server, client):
+        check = bool(os.path.isdir(os.path.expanduser(server)) and os.path.isdir(os.path.expanduser(client)))
+        if check:
+            list_client = os.listdir(os.path.expanduser(client))
+            for file in os.listdir(os.path.expanduser(server)):
+                f = file
+                if not f in list_client:
+                    os.chdir(os.path.expanduser(server))
+                    shutil.copy2(f, os.path.expanduser(client))
+                    print('--> Copying ' + f)
+            print('::Client sync complete without errors')
 
-    def server_sync(dir1, dir2):
-    # stesso dell'altro ma al contrario
+    def server_sync(server, client):
+        check = bool(os.path.isdir(os.path.expanduser(server)) and os.path.isdir(os.path.expanduser(client)))
+        if check:
+            list_server = os.listdir(os.path.expanduser(server))
+            for file in os.listdir(os.path.expanduser(client)):
+                f = file
+                if not f in list_server:
+                    os.chdir(os.path.expanduser(client))
+                    shutil.copy2(f, os.path.expanduser(server))
+                    print('--> Copying ' + f)
+            print('::Server sync complete without errors')
 
     def global_sync(dir1, dir2):
     # copia i file che mancano in una cartella nell'altra e viceversa
